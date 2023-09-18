604f8947f0cd514d6cfd6eab1decf8eca8dbe06e
Kelvin <kelvin0898@live.com>
2023-09-18 21:50:37 +1000

    organizing code

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 import googlemaps
 import os
 
+### API_KEY from environment variable
+api_key = os.environ.get("API_KEY")
+map_client = googlemaps.Client(api_key)
+
+### Initializing FastAPI as decider
 decider = FastAPI(
     title = "Decide what to EAT"
 )
 
-api_key = os.environ.get("API_KEY")
-map_client = googlemaps.Client(api_key)
+location_name = "Oiden"
 
 def get_details(restaurant_name):
     response = map_client.places(query=location_name)
@@ -17,10 +21,8 @@ def get_details(restaurant_name):
     rating = response["results"][0]["rating"]
     return {"Restaurant name": restaurant_name, "Address": address, "Open": open_now, "Rating": rating}
 
-location_name = "Oiden"
-
 @decider.get("/")
-async def getRestaurant():
+async def get_restaurant():
     return get_details(location_name)
 
 @decider.get("/me")
