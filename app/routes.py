0a08a613555b585b5b8725868f914f1c8a80755f
Kelvin <kelvin0898@live.com>
2023-10-25 21:32:33 +1100

    adding new routes and use TinyDB to get data from data.json file

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,37 @@
 # This is a routes script which directs the request
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 
+from config import Settings
+from crud import CRUD
+
+settings = Settings()
 # Initializaing a Jinja template object and pointing out to a template directory
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=settings.TEMPLATE_DIR)
 router = APIRouter()
 router2 = APIRouter()
 
 
 @router.get("/hello")
-def index(request: Request):
-    return templates.TemplateResponse("main.html",
+def hello(request: Request):
+    return templates.TemplateResponse("shared/_base.html",
                                       {"request": request,
                                        "name": "Kelvin",
                                        "age": 25})
 
+@router.get("/test")
+def index(request: Request):
+
+    db = CRUD().with_table("artist_info")
+    random_artist = db.get_random_item()
+
+    return templates.TemplateResponse(
+        "main.html",
+        {
+            "request": request,
+            "random_artist": random_artist})
+
 
 @router2.get("/me")
-async def myself():
-    return {"Hello, my name is Kelvin."}
+def myself():
+    return "Hello, my name is Kelvin."
