7695bbe7940a6179b1a5d42b8fbcb61e45e3c548
Kelvin <kelvin0898@live.com>
2023-10-25 21:31:56 +1100

    commenting the google places API for now to accomodate for the traiaing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
+import subprocess
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
+
 from routes import router, router2
+
+from config import Settings
+from crud import CRUD
+
 import googlemaps
 import os
 import orjson
@@ -9,37 +17,68 @@ import orjson
 api_key = os.environ.get("API_KEY")
 map_client = googlemaps.Client(api_key)
 
-# Initializing FastAPI as decider
-decider = FastAPI(
-    title="Decide what to EAT"
-)
+settings = Settings()
 
-# Mounting the main.py to a static folder which has the css configuration
-decider.mount("/static", StaticFiles(directory="static"), name="static")
-decider.include_router(router)
-decider.include_router(router2)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Context manager for FastAPI app. It will run all code before `yield`
+    on app startup, and will run code after `yeld` on app shutdown.
+    """
+
+    try:
+        subprocess.run([
+            "tailwindcss",
+            "-i",
+            str(settings.STATIC_DIR / "src" / "input.css"),
+            "-o",
+            str(settings.STATIC_DIR / "main.css"),
+        ])
+    except Exception as e:
+        print(f"Error running tailwindcss: {e}")
+
+    yield
+
+def get_app() -> FastAPI:
+    """Create a FastAPI app with the specified settings."""
+    app = FastAPI(
+        lifespan=lifespan,
+        **settings.fastapi_kwargs)
+    
+    # Mounting the main.py to a static folder which has the css configuration
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    app.include_router(router)
+    app.include_router(router2)
+
+    return app
+
+decider = get_app()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(decider, host="127.0.0.1", port=8000)
+
+# class CustomORJSONResponse(Response):
+#     media_type = "application/json"
+
+#     def render(self, content: any) -> bytes:
+#         assert orjson is not None, "orjson must be installed"
+#         return orjson.dumps(content, option=orjson.OPT_INDENT_2)
 
 
-class CustomORJSONResponse(Response):
-    media_type = "application/json"
-
-    def render(self, content: any) -> bytes:
-        assert orjson is not None, "orjson must be installed"
-        return orjson.dumps(content, option=orjson.OPT_INDENT_2)
+# location_name = "eskrim angie"
 
 
-location_name = "eskrim angie"
+# def get_details(restaurant_name):
+#     response = map_client.places(query=location_name)
+#     restaurant_name = response["results"][0]["name"]
+#     address = response["results"][0]["formatted_address"]
+#     open_now = response["results"][0]["opening_hours"].get("open_now")
+#     rating = response["results"][0]["rating"]
+#     return {"Restaurant name": restaurant_name, "Address": address, "Open": open_now, "Rating": rating}
 
 
-def get_details(restaurant_name):
-    response = map_client.places(query=location_name)
-    restaurant_name = response["results"][0]["name"]
-    address = response["results"][0]["formatted_address"]
-    open_now = response["results"][0]["opening_hours"].get("open_now")
-    rating = response["results"][0]["rating"]
-    return {"Restaurant name": restaurant_name, "Address": address, "Open": open_now, "Rating": rating}
-
-
-@decider.get("/", response_class=CustomORJSONResponse)
-async def get_restaurant():
-    return get_details(location_name)
+# @decider.get("/", response_class=CustomORJSONResponse)
+# async def get_restaurant():
+#     return get_details(location_name)
