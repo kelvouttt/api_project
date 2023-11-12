411cc3e76adfac6ada81bddc49eb1491d3603bb2
Kelvin <kelvin0898@live.com>
2023-11-12 21:05:34 +1100

    moving function as module

---
+++ get_coordinates.py
@@ -0,0 +1,9 @@
+import random_points as rp
+import range
+
+## Get coordinate x and y
+def get_coordinates():
+    location = rp.polygon_random_points(range.poly, 1)
+    # The way it was done is by returning both coordinates.
+    for coordinates in location:
+        return coordinates.x, coordinates.y
