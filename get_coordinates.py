697c90a89305eab89a3a92baa09c7fa24da6c98e
Kelvin <kelvin0898@live.com>
2023-11-20 17:32:48 +1100

    moving this file to function folder

--- get_coordinates.py
+++
@@ -1,9 +0,0 @@
-import random_points as rp
-import range
-
-## Get coordinate x and y
-def get_coordinates():
-    location = rp.polygon_random_points(range.poly, 1)
-    # The way it was done is by returning both coordinates.
-    for coordinates in location:
-        return coordinates.x, coordinates.y
