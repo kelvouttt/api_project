b457f4b298038c2912fcea711ee0b4aeb9b58ab2
Kelvin <kelvin0898@live.com>
2023-11-20 17:34:05 +1100

    moving these files to a function folder

--- range.py
+++
@@ -1,8 +0,0 @@
-from shapely.geometry import Polygon, Point
-
-## Define desired Polygon
-# https://medium.com/the-data-journal/a-quick-trick-to-create-random-lat-long-coordinates-in-python-within-a-defined-polygon-e8997f05123a
-poly = Polygon([(-33.822218,151.084762), 
-                (-33.919981,151.099747),
-                (-33.917772,151.240542),
-                (-33.872596, 151.262135)])
