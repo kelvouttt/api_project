20bd382ddc01a8bd08d8231bd9fb05fcfcf5c3ca
Kelvin <kelvin0898@live.com>
2023-11-28 11:27:43 +1100

    adding new router to search by post code, currently using Annotated

--- routes.py
+++ routes.py
@@ -2,10 +2,12 @@ from dataclasses import dataclass
 from fastapi import Depends, Form, Response, Request, APIRouter
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from function import function as details
+from typing_extensions import Annotated
 
 import orjson
 import function.random_points as rp
-import function.restaurants as details
+
 
 decider = APIRouter()
 
@@ -15,6 +17,11 @@ templates = Jinja2Templates(directory="templates")
 @dataclass
 class Restaurant:
     name: str = Form(...)
+
+# Creating a new class for postal code post request, for some reason, I can't instantiate new object using the existing class Restaurant. So as a workaround, will try to create new object instead to make it work.
+# @dataclass 
+# class Postal:
+#     postcode: int = Form(...)
 
 
 ## Defining JSON Response object
@@ -27,23 +34,24 @@ class CustomORJSONResponse(Response):
 
 ### Routes
 
-## Will return whatever places we pass into the query parameters
+# Will return whatever places we pass into the query parameters
 @decider.get("/restaurant/{places_to_eat}", 
              response_class=CustomORJSONResponse)
 async def get_restaurant(places_to_eat):
-    return details.return_restaurant(places_to_eat)  
+    return details.return_restaurant(places_to_eat) 
 
 ## We can submit a form, and it will return place details we put in
-@decider.post("/submit/", response_class=HTMLResponse)
+@decider.post("/submit/", 
+              response_class=HTMLResponse)
 async def submit(request: Request, restaurant: Restaurant = Depends()):
     results_function: dict = details.response(restaurant)
 
-    return templates.TemplateResponse("response.html", {"request": request,
-                                                        "restaurant_name": results_function["Restaurant name"],
-                                                        "address": results_function["Address"],
-                                                        "status": results_function["Open"],
-                                                        "rating": results_function["Rating"]      
-                                                        })
+    return templates.TemplateResponse("submit_response.html", {"request": request,
+                                                                "restaurant_name": results_function["Restaurant name"],
+                                                                "address": results_function["Address"],
+                                                                "status": results_function["Open"],
+                                                                "rating": results_function["Rating"]      
+                                                                })
 
 ## Will get new restaurants every refresh but response is in JSON format
 @decider.get("/restaurantsJSON", 
@@ -52,7 +60,7 @@ async def get_restaurant():
     return details.get_details()
 
 ## Return restaurants every refresh and response in HTMl page
-@decider.post("/restaurants", 
+@decider.post("/restaurants/", 
              response_class=HTMLResponse)
 async def get_restaurant(request: Request):
     random_restaurant: dict = details.get_details()
@@ -64,9 +72,25 @@ async def get_restaurant(request: Request):
                                                         "address": random_restaurant["Address"],
                                                         "status": random_restaurant["Open"],
                                                         "rating": random_restaurant["Rating"],
+                                                        ## This static API is being called out in response.html to avoid hard-coding the API key in this file
                                                         "static_api": static_api     
                                                         })
 
+@decider.post("/postalcode/", 
+              response_class=HTMLResponse)
+async def submit_form(request: Request, postalcode: Annotated[int, Form()]):
+    function_result: dict = details.input_response(postalcode)
+    print(function_result["Address"])
+    return templates.TemplateResponse("postal_response.html", {"request": request,
+                                                            "address": function_result["Address"]    
+                                                            })
+
+
+@decider.get("/postal/{postcode}", 
+             response_class=CustomORJSONResponse)
+async def get_restaurant(postcode):
+    return details.input_response(postcode)  
+ 
 # @decider.get("/")
 # async def myself():
 #     return {"hello, my name is Kelvin."}
