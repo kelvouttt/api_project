7db56d3edfb8d95960c4a7c3e5e04e4d0be64549
Kelvin <kelvin0898@live.com>
2023-11-28 12:20:24 +1100

    adding functionality to return latitude and longitude from the postcode request

--- function/function.py
+++ function/function.py
@@ -51,7 +51,9 @@ def input_response(postcode: int):
     response = static_map_client.geocode(components={
                                         "country": "AU","postal_code":postcode})
     format = response[0]
- 
+
     address = format["formatted_address"]
+    lat = format["geometry"]["location"]["lat"]
+    lng = format["geometry"]["location"]["lng"]
     
-    return {"Address": address}
+    return {"Address": address, "Latitude": lat, "Longitude": lng}
